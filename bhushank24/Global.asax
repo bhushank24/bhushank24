ca039d0124f0b70465f273a1253b650487d9adfa
Bhushan Kulkarni <bhushan.kulkarni.24@outlook.com>
2018-11-03 07:53:37 +0000

    COmmiting changes

---
+++ bhushank24/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="bhushank24.MvcApplication" Language="C#" %>
---
+++ bhushank24/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(bhushank24.Startup))]
+namespace bhushank24
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
